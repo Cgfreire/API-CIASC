74488bb6ce28228bbe8d3e0e2c97edd6e42f908c
Caio G. Freire <88165211+Cgfreire@users.noreply.github.com>
2023-05-22 21:42:30 -0300

    Adicionando os métodos restantes, corrigir PUT

--- CIASC/src/main/java/com/br/ciasc/controller/PacienteController.java
+++ CIASC/src/main/java/com/br/ciasc/controller/PacienteController.java
@@ -4,35 +4,49 @@ import com.br.ciasc.models.Paciente;
 import com.br.ciasc.repository.PacienteRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-public class PacienteController {
 @RestController
 @RequestMapping(value = "/ciasc")
-public class GameController {
+public class PacienteController{
 
     @Autowired
-    private PacienteRepository pacienteRepository;
+    PacienteRepository pacienteRepository;
 
     @GetMapping(value = "/pacientes")
     public List<Paciente> lista_pacientes(){
         return pacienteRepository.findAll();
         }
-    }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+
+    @PostMapping(value = "/cadastro")
     public Paciente cadastrar(@RequestBody Paciente paciente){
         return pacienteRepository.save(paciente);
     }
     
-    @PutMapping(value = "/altera_paciente/{id}")
+    @PutMapping(value = "/atualiza/{id}")
+    public ResponseEntity<Paciente> atualiza(@PathVariable(value = "id") Long pacienteId, @RequestBody Paciente novo_paciente) {
+        novo_paciente.setId(novo_paciente.getId());
+        novo_paciente.setNome(novo_paciente.getNome());
+        final Paciente updatePaciente = pacienteRepository.save(novo_paciente);
+        return ResponseEntity.ok(updatePaciente);
+    }
+
     
-    
-    @DeleteMapping(value = "/apaga_paciente/ {id}")
+    @DeleteMapping("/exclui/{id}")
+    public String exclui(@PathVariable("id") Long id) {
+        if (id != null) {
+            pacienteRepository.deleteById(id);
+        return "Excluído";
+            }
+    return null;
+    }
 }
